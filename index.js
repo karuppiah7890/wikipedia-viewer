1ae965a433c461b1e1a9f80cdc354f95b4ef47ad
Karuppiah <karuppiah7890@gmail.com>
2016-05-13 14:12:17 +0530

    Add auto-complete module for search

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 $(document).ready(function(){
 
+    $("#search_input").autocomplete({
+      source : function(request,dataCallback) {
+                  $.ajax({
+                      method : "GET",
+                      url : "http://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=http%3A%2F%2Fkaruppiah7890.github.io&search=" + request.term + "&limit=10&formatversion=2",
+                      dataType : "json",
+                      success : function(response) {
+                        dataCallback(response[1]);
+                      },
+                      error : function(){
+                          dataCallback([]);
+                      }
+                  });
+              },
+
+      select : function(event,ui) {
+                  if(event.keyCode!=13)
+                  {
+                    //console.log("autocomplete event fired!");
+                    searchWiki(ui.item.value);
+                  }
+              }
+    });
+
     $("#search_input").keyup(function(event){
         if(event.keyCode==13)
-        searchWiki($("#search_input").val());
+        {
+          //console.log("enter select fired");
+          $("#search_input").autocomplete("close");
+          searchWiki($("#search_input").val());
+        }
     });
 
     $("#search_go").on("click",function(){
+      //console.log("button event fired");
       searchWiki($("#search_input").val());
     });
 });
@@ -17,14 +46,17 @@ function searchWiki(query) {
     dataType : "json",
     success : showResults,
     error : function(val) {
-      console.log(val);
+      console.log("Error occurred while fetching results for the query");
+      $(".alert").remove();
+      var html = "<div class=\"alert alert-danger\" role=\"alert\">Oops! Something went wrong! Check your internet connection and try again!</div>";
+      $("#search_results").append(html);
     }
   });
 }
 
 function showResults(response) {
 
-    console.log(response);
+    //console.log(response);
 
     var allResults = response.query.search;
 
