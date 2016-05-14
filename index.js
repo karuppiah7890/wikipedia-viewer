1a72450b2389bbb96c8ad174c022bcdb695a3b1a
Karuppiah <karuppiah7890@gmail.com>
2016-05-14 15:51:20 +0530

    Fix empty query request

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ $(document).ready(function(){
 
     $("#search_input").autocomplete({
       source : function(request,dataCallback) {
-                  $.ajax({
-                      method : "GET",
-                      url : "http://en.wikipedia.org//w/api.php?action=opensearch&format=json&search=" + request.term + "&limit=10&formatversion=2",
-                      dataType : "jsonp",
-                      success : function(response) {
-                        dataCallback(response[1]);
-                      },
-                      error : function(){
-                          dataCallback([]);
-                      }
-                  });
+                  if($.trim(request.term)!=="") {
+                        $.ajax({
+                            method : "GET",
+                            url : "http://en.wikipedia.org//w/api.php?action=opensearch&format=json&search=" + request.term + "&limit=10&formatversion=2",
+                            dataType : "jsonp",
+                            success : function(response) {
+                              dataCallback(response[1]);
+                            },
+                            error : function(){
+                                dataCallback([]);
+                            }
+                        });
+                   }
               },
 
       select : function(event,ui) {
@@ -42,18 +44,21 @@ $(document).ready(function(){
 });
 
 function searchWiki(query) {
-  $.ajax({
-    method : "GET",
-    url : "http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&formatversion=2&srsearch=" + query + "&srprop=snippet&srlimit=10",
-    dataType : "jsonp",
-    success : showResults,
-    error : function(val) {
-      console.log("Error occurred while fetching results for the query");
-      $(".alert").remove();
-      var html = "<div class=\"alert alert-danger\" role=\"alert\">Oops! Something went wrong! Check your internet connection and try again!</div>";
-      $("#search_results").append(html);
-    }
-  });
+
+  if($.trim(query)!=="") {
+      $.ajax({
+        method : "GET",
+        url : "http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&formatversion=2&srsearch=" + query + "&srprop=snippet&srlimit=10",
+        dataType : "jsonp",
+        success : showResults,
+        error : function(val) {
+          console.log("Error occurred while fetching results for the query");
+          $(".alert").remove();
+          var html = "<div class=\"alert alert-danger\" role=\"alert\">Oops! Something went wrong! Check your internet connection and try again!</div>";
+          $("#search_results").append(html);
+        }
+      });
+  }
 }
 
 function showResults(response) {
